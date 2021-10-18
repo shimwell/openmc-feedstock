8205a5e544efcb503b2dbdcfce6a929899731fa4
Jonathan Shimwell <mail@jshimwell.com>
2021-10-18 13:25:10 +0100

    adding dagmc compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,23 @@
 set -x
 set -e
 
-# Build and install executable
+# Build and install DAGMC
+cd vendor
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      ..
+make -j "${CPU_COUNT}"
+make install
+cd ..
+
+# Build and install OpenMC executable
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release \
       -DHDF5_ROOT="${PREFIX}" \
+      -Ddagmc=ON \
+      -DDAGMC_ROOT="vendor/${PREFIX}" \
       ..
 make -j "${CPU_COUNT}"
 make install
